c05de9b4edf17e5a451e254e1bdeb1cc4dce62d8
VarkoMac <cmendozasoberanis@gmail.com>
2024-03-14 18:49:46 -0600

    [UPDATE] Pasarle la carpeta para leer su contenido

--- native_modules/elese/elese.js
+++ native_modules/elese/elese.js
@@ -4,7 +4,15 @@
 
 const fs = require("node:fs/promises");
 
-fs.readdir(".")
+// 03/14/2024 - 18:44
+// Poder pasar como parámetro dentro de la linea de comandos la carpeta en la cual queremos usar el 'ls'
+// Ejemplo, si ejecutamos node elese/elese.js ---> obtendremos todo el contenido de elese/
+// Ejemplo, si ejecutamos node elese/elese.js path ---> obtendremos unicamente el contenido de path/
+// Ejemplo, si ejecutamos node elese/elese.js os ---> obtendremos unicamente el contenido de os/
+// Ejemplo, si ejecutamos node elese/elese.js random ---> obtendremos "Error al leer el directorio: ENOENT", ya que random/ no existe
+const folder = process.argv[2] ?? '.'
+
+fs.readdir(folder)
   .then((files) => {
     files.forEach((file) => {
       console.log(file);
